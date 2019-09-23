f3ea0615138f015c933d94565d2ab0d1ef487897
waxime <aveline.maxime@gmail.com>
2019-09-23 21:15:17 +0200

    WIP try things for api routes

--- main.go
+++ main.go
@@ -1,7 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type user struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Votes     int    `json:"user_votes"`
+}
+
+type allUsers []user
+
+var users = allUsers{
+	{
+		ID:        1,
+		FirstName: "Waxime",
+		LastName:  "AVELINE",
+		Email:     "aveline.maxime@gmail.com",
+		Votes:     0,
+	},
+}
 
 func main() {
-	fmt.Println("This is golang-vote-api")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/event", createEvent).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8001", router))
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser user
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "The user was created ;)")
+	} else {
+		fmt.Fprintf(w, "Woupsy doopsy")
+	}
+
+	json.Unmarshal(reqBody, &newUser)
+	users = append(users, newUser)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newUser)
+}
+
+// EVENT TEST
+type event struct {
+	ID          int    `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          1,
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+	},
+}
+
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
 }
